b383b02bda33bd6aa04493f182c21998da1b0ed8
ericx20 <ericxu2048@gmail.com>
2020-08-06 22:00:26 -0400

    implemented stroke history

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,79 @@
 const clearButton = document.querySelector('.clear');
+const undoButton = document.querySelector('.undo');
+const redoButton = document.querySelector('.redo');
 const stroke_weight = document.querySelector('.stroke-weight');
-const color_picker = document.querySelector('.color-picker')
+const color_picker = document.querySelector('.color-picker');
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-let isDrawing = false;
+
+var isDrawing = false;
+const strokeList = [];
+var newLine = new Path2D();
 
 canvas.addEventListener('mousedown', start);
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', stop);
+addEventListener('keydown', testing)
 
+window.addEventListener('resize', resizeCanvas);
 clearButton.addEventListener('click', clearCanvas);
+// undoButton.addEventListener('click', console.log('undo lol'));
+// redoButton.addEventListener('click', console.log('redo lol'));
+
+
+// display the newLine when you press F
+function testing (e) {
+    if (e.code === 'KeyF') {
+        console.log('prev stroke has been displayed');
+        ctx.stroke(newLine);
+    } else if (e.code === 'KeyG') {
+        console.log('testing G');
+        console.log(strokeList.length)
+        strokeList.forEach(line => {
+            ctx.beginPath();
+            ctx.stroke(line);
+            ctx.beginPath();
+        })
+    }
+}
 
 function start (e) {
     isDrawing = true;
+    newLine = new Path2D();
+    console.log("start");
     draw(e);
 }
 
 function draw ({ clientX: x, clientY: y }) {
     if (!isDrawing) return;
+
     ctx.lineWidth = stroke_weight.value;
     ctx.lineCap = "round";
     ctx.strokeStyle = color_picker.value;
 
-    ctx.lineTo(x, y)
+    newLine.lineTo(x, y);
+
+    ctx.lineTo(x, y);
     ctx.stroke();
     ctx.beginPath();
-    ctx.moveTo(x, y)
+    ctx.moveTo(x, y);
 }
 
 function stop() {
     isDrawing = false;
+    console.log("stop");
     ctx.beginPath();
+    strokeList.push(newLine);
+    
 }
 
 function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    console.log("clear");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-window.addEventListener('resize', resizeCanvas);
+
 function resizeCanvas () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
