0674d9356d4a93057fc2cf984287d3a0a083ffda
ericx20 <ericxu2048@gmail.com>
2020-08-14 02:03:35 -0400

    undo and export to PNG added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,30 @@
 const clearButton = document.querySelector('.clear');
 const undoButton = document.querySelector('.undo');
-const redoButton = document.querySelector('.redo');
 const downloadButton = document.querySelector('.download')
-// const eraseButton = document.querySelector('.erase')
+const eraseButton = document.querySelector('.erase')
 const stroke_weight = document.querySelector('.stroke-weight');
 const color_picker = document.querySelector('.color-picker');
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+const WIDTH = 624;
+const HEIGHT = 800;
+
 var isDrawing = false;
 const strokeList = [];
 var newLine = new Path2D();
 
+
+// event listeners
 canvas.addEventListener('mousedown', start);
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', stop);
 addEventListener('keydown', testing)
 
-window.addEventListener('resize', resizeCanvas);
+// window.addEventListener('resize', resizeCanvas);
 clearButton.addEventListener('click', clearCanvas);
-undoButton.addEventListener('click', clearCanvas);
-redoButton.addEventListener('click', clearCanvas);
+undoButton.addEventListener('click', undo);
 downloadButton.addEventListener('click', downloadImage);
 
 // display the newLine when you press F
@@ -31,22 +34,24 @@ function testing (e) {
         ctx.stroke(newLine);
     } else if (e.code === 'KeyG') {
         console.log('testing G');
-        console.log(strokeList.length)
-        strokeList.forEach(line => {
-            ctx.stroke(line);
-        })
+        ctx.strokeStyle = "#FF0000";
+        ctx.stroke(strokeList[strokeList.length-1]);
+        console.log(JSON.stringify(strokeList[0]));
     }
 }
 
-function start (e) {
+function start(e) {
     isDrawing = true;
     newLine = new Path2D();
     console.log("start");
     draw(e);
 }
 
-function draw ({ clientX: x, clientY: y }) {
+function draw({ clientX, clientY}) {
     if (!isDrawing) return;
+
+    x = clientX - (window.innerWidth - WIDTH) / 2;
+    y = clientY - (window.innerHeight - HEIGHT) / 2;
 
     ctx.lineWidth = stroke_weight.value;
     ctx.lineCap = "round";
@@ -71,18 +76,33 @@ function stop() {
 
 function clearCanvas() {
     console.log("clear");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+}
+
+function undo() {
+    strokeList.pop();
+    clearCanvas()
+    strokeList.forEach(line => {
+        ctx.stroke(line);
+    })
 }
 
 
-function resizeCanvas () {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
+// function resizeCanvas () {
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;
+// }
 
-function downloadImage () {
-    let dataURL = canvas.toDataURL('image/png')
-    console.log(dataURL)
+function downloadImage() {
+    let dataURL = canvas.toDataURL('image/png');
+    downloadButton.href = dataURL;
+    console.log(dataURL);
 }
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
 
-resizeCanvas();
+// resizeCanvas();
+ctx.fillStyle = "#FFFFFF";
+ctx.fillRect(0, 0, WIDTH, HEIGHT);
