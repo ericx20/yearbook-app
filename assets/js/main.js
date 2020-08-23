57279111835e6e8ff305d4b3d39b735e6729e4b3
ericx20 <ericxu2048@gmail.com>
2020-08-23 00:42:41 -0400

    undo/redo for clearing canvas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,11 @@
 /* TO-DO LIST
- - get clearing and unclearing canvas working
  - add coords to path object for converting to JSON without loss of path data
  - start working on node backend
-
-   THOUGHTS
- - should addLine and removeLine be combined into one function? (something like runCommand() 
-   shares a lot of code and whether it's add/remove can be made into argumnet
-   and it gets rid of 'if add then add else if remove then remove' logic
-
 */
 
-// also turns out can't convert path2d to json, therefore...
-// will record x & y while drawing into array, add that as custom property to path2d
-// when sending off to server, just send the x & y arrays of all strokes + color, weight, etc
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const clearButton = document.querySelector('.clear');
-// const undoButton = document.querySelector('.undo');
 const downloadButton = document.querySelector('.download');
 const penButton = document.querySelector('.pen');
 const eraseButton = document.querySelector('.erase');
@@ -38,7 +26,7 @@ var currentColor = '#171717';
 var currentLine = new Path2D();
 
 // history
-var lineList = [];  // all lines currently drawn to the canvas
+var lineList = [];   // all lines currently drawn to the canvas
 var undoStack = [];  // history of all actions
 var redoStack = [];  // history of undone actions that may be redone
 
@@ -52,12 +40,13 @@ addEventListener('keydown', hotKeys)
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 ctx.lineCap = 'round';
+drawWhiteRect();
+activatePen();
 
 // -------- TOOLS -------- //
 
 // sets pen as active tool
 function activatePen() {
-    console.log('PEN ACTIVATED');
     activeTool = 'pen';
     penButton.style.background = YELLOW;
     eraseButton.style.background = WHITE;
@@ -65,7 +54,6 @@ function activatePen() {
 
 // sets eraser as active tool
 function activateEraser() {
-    console.log('ERASER ACTIVATED');
     activeTool = 'eraser';
     penButton.style.background = WHITE;
     eraseButton.style.background = YELLOW;
@@ -84,12 +72,12 @@ function hotKeys(e) {
             break;
         case 'KeyZ':
             if (e.ctrlKey) {
-                undo();
+                undo();  // Ctrl+Z
             }
             break;
         case 'KeyY':
             if (e.ctrlKey) {
-                redo();
+                redo();  // Ctrl+Y
             }
     }
 }
@@ -99,7 +87,6 @@ function hotKeys(e) {
 
 // starts drawing
 function start(e) {
-    console.log('start');
     isDrawing = true;
     if (activeTool === 'pen') {
         currentLine = new Path2D();
@@ -132,11 +119,11 @@ function draw({ clientX, clientY }) {
         ctx.beginPath();
         ctx.moveTo(x, y);
 
-    } else {  // eraser time
+    } else {  // if pen isn't active, then eraser is active
         for (i = lineList.length - 1; i >= 0; i--) {
             var stroke = lineList[i];
+
             if (ctx.isPointInStroke(stroke, x, y)) {
-                console.log('hit');
                 removeLine(stroke, i);
                 return;
             }
@@ -147,13 +134,13 @@ function draw({ clientX, clientY }) {
 
 // stops drawing & saves current line
 function stop() {
-    console.log('stop');
     isDrawing = false;
 
     if (activeTool === 'pen') {
         ctx.beginPath();
         currentLine.color = currentColor;
         currentLine.weight = currentWeight;
+
         // add currentLine to the top of the lineList
         addLine(currentLine, lineList.length);
     }
@@ -167,78 +154,119 @@ function drawWhiteRect() {
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
 }
 
-// redraws canvas from list of lines
-function redrawCanvas() {
-    drawWhiteRect();
-    lineList.forEach(line => {
-        ctx.lineWidth = line.weight;
-        ctx.strokeStyle = line.color;
-        ctx.stroke(line);
-    })
-}
-
-// clears everything from canvas
-function clearCanvas() {
-    console.log('CLEAR DISABLED TEMPORARILY');
-
-    // whiteRectangle = new Path2D();
-    // whiteRectangle.rect(0, 0, WIDTH, HEIGHT);
-    // whiteRectangle.color = WHITE;
-    // // lineList.push(whiteRectangle);
-
-    // drawWhiteRect();
-}
-
-// -------- COMMANDS, UNDO, REDO -------- //
-// command consists of the following:
-// { command: string, line: Path2D, index: int}
-
-// addLine adds a new line at specified index of lineList
-
-function addLine(line, index, byUser = true) {
-    lineList.splice(index, 0, line)
+// redraws a line with its original style
+function redraw(line) {
+    console.log('REDRAW'); // DEBUGGING
+    console.log(line);
 
     ctx.lineWidth = line.weight;
     ctx.strokeStyle = line.color;
     ctx.stroke(line);
+}
+
+// redraws canvas from list of lines
+function redrawCanvas() {
+    drawWhiteRect();
+    if (lineList.length > 0) {
+        lineList.forEach(line => redraw(line));
+    }
+}
+
+// clears everything from canvas
+function clearCanvas() {
+    if (lineList.length === 0) {
+        // console.log('NOTHING TO CLEAR');
+        return;
+    }
+    // console.log('CLEAR');
+
+    removeManyLines([...lineList], 0);
+
+    drawWhiteRect();
+}
+
+// downloads canvas as image
+function downloadImage() {
+    let dataURL = canvas.toDataURL('image/png');
+    downloadButton.href = dataURL;
+}
+
+// -------- COMMANDS, UNDO, REDO -------- //
+
+// adds one line at specified index of lineList
+function addLine(line, index, byUser = true) {
+    lineList.splice(index, 0, line);
+
+    // make it appear on canvas
+    redraw(line);
 
     if (byUser) {
-        console.log('added line by user');
+        // console.log('added line by user');
         undoStack.push({ command: 'add', line, index });
         redoStack = [];
     }
 }
 
-// removeLine removes existing line at specified index of lineList
+// removes one line at specified index of lineList
 function removeLine(line, index, byUser = true) {
     lineList.splice(index, 1);
-    
-    redrawCanvas();
+
+    redrawCanvas()
     
     if (byUser) {
-        console.log('removed line by user')
+        // console.log('removed line by user');
         undoStack.push({ command: 'remove', line, index });
         redoStack = [];
     }
 }
 
 
-// function addLines(lines) {
+// adds many lines starting at specified index
+// NEVER called by user, only exists as inverse to removeManyLines
+function addManyLines(lines, index) {
+    lineList.splice(index, lines.length, ...lines);
+    lines.forEach(line => redraw(line));
+}
 
-// }
+// removes many lines starting at specified index
+function removeManyLines(lines, index, byUser = true) {
+    lineList.splice(index, lines.length);
+    redrawCanvas();
+
+    if (byUser) {
+        undoStack.push({ command: 'removeMany', lines, index });
+        redoStack = [];
+    }
+
+}
 
 function undo() {
+    if (undoStack.length === 0) {
+        console.log('CANNOT UNDO');
+        return;
+    }
+
     console.log('undo');
     undoCommand = undoStack.pop();
     redoStack.push(undoCommand);
 
     // execute the inverse of the command, NOT as user
     // as to not add this execution to any undo/redo stacks
-    const {command, line, index} = undoCommand;
-    if (command === 'add') {
-        removeLine(line, index, false);
-    } else if (command === 'remove') {
-        addLine(line, index, false);
+    const {command, index} = undoCommand;
+
+    // make this a switch later on (same for redo()), when adding addMany/removeMany
+    switch (command) {
+        case 'add':
+            removeLine(undoCommand.line, index, false);
+            break;
+        case 'remove':
+            addLine(undoCommand.line, index, false);
+            break;
+        case 'addMany':
+            removeManyLines(undoCommand.lines, index, false);
+            break;
+        case 'removeMany':
+            addManyLines(undoCommand.lines, index);
     }
 }
 
@@ -253,21 +281,19 @@ function redo() {
     undoStack.push(redoCommand);
 
     // execute the command NOT as user
-    const {command, line, index} = redoCommand;
-    if (command === 'add') {
-        addLine(line, index, false);
-    } else if (command === 'remove') {
-        removeLine(line, index, false);
+    const {command, index} = redoCommand;
+
+    switch (command) {
+        case 'add':
+            addLine(redoCommand.line, index, false);
+            break;
+        case 'remove':
+            removeLine(redoCommand.line, index, false);
+            break;
+        case 'addMany':
+            addManyLines(redoCommand.lines, index, false);
+            break;
+        case 'removeMany':
+            removeManyLines(redoCommand.lines, index, false);
     }
-    
 }
-
-function downloadImage() {
-    console.log('download');
-    let dataURL = canvas.toDataURL('image/png');
-    downloadButton.href = dataURL;
-}
-
-
-drawWhiteRect();
-activatePen();
