46dce5122184513c832711204af61df13dc12ff3
Eric Xu <ericxu2048@gmail.com>
2023-05-14 16:30:04 -0400

    Hello world + fix CORS issue

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script>
+	import { onMount } from 'svelte';
+
+    let testMessage = "" 
+    async function getTestMessage() {
+        try {
+            const response = await fetch("http://localhost:3000/")
+            testMessage = await response.text()
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    onMount(getTestMessage);
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>The message: {testMessage}</p>
--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,19 @@
 import express, { Express } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv'
 
 dotenv.config();
 
 const app: Express = express()
 const port = process.env.PORT || 3000
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(', ') ?? []
+
+console.log(allowedOrigins)
+
+app.use(cors({
+  origin: allowedOrigins,
+}))
 
 app.get('/', (req, res) => {
   res.send('Hi mom!')
